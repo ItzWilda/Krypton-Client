3bbedc78a0876a925612a22d2b42f54a505c8d2a
ItzWilda <168257884+ItzWilda@users.noreply.github.com>
2025-05-14 10:55:54 -0400

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const script = document.createElement('script');
+  script.src = "https://yourdomain.com/krypton.js";
+  document.head.appendChild(script);
+});
